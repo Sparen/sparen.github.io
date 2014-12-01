f64fe87bedc9f12771ea43e13167ac54d224d2d9
Andrew Fan <andrewsuicune@gmail.com>
2014-12-01 17:20:45 -0500

    Changes

--- menu.php
+++
@@ -1,5 +0,0 @@
-<ul>
-  <li><a class="mhome" href="./home.html">Home</a></li>
-  <li><a class="mabout" href="./aboutme.html">About Me</a></li>
-  <li><a class="mlocaafanfiction" href="./locaafanfiction.html">LOCAA Fanfiction</a></li>
-</ul>
