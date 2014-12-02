86f36a7080b097da016ed8ed5ac720cda602fa81
Andrew Fan <andrewsuicune@gmail.com>
2014-12-02 16:19:21 -0500

    Tutorial 3 done

---
+++ ph3tutorials/javascript/U1L3Quiz1.js
@@ -0,0 +1,31 @@
+function checkAnswer(quizForm, theAnswer){
+  var s = "?";
+
+  var i = 0;
+  for(;i<quizForm.elements.length;i++){
+    if(("cc" == quizForm.elements[i].name) && (quizForm.elements[i].checked)){
+      s = quizForm.elements[i].value;
+    }
+  }
+
+  if(s == "?"){
+    alert("Please make a selection.");
+    return false;
+  }
+
+  if(s == theAnswer){
+    alert("'"+s+"' is correct! You should make nonspells and spells in Single scripts!");
+  }else if(s == "A"){
+    alert("'"+s+"' is incorrect. Please review Part 1.");
+  }else{
+    alert("'"+s+"' is incorrect. Plural scripts are used to chain together attacks and are used in boss battles. Please review Part 1.");
+  }
+
+  // return "false" to indicate not to
+  // submit the form.
+  // change this to "true" if the form
+  // "action" is valid,
+  // i.e. points to a valid CGI script
+  //
+  return false;
+}
---
+++ ph3tutorials/javascript/U1L3Quiz2.js
@@ -0,0 +1,33 @@
+function checkAnswer(quizForm, theAnswer){
+  var s = "?";
+
+  var i = 0;
+  for(;i<quizForm.elements.length;i++){
+    if(("cc" == quizForm.elements[i].name) && (quizForm.elements[i].checked)){
+      s = quizForm.elements[i].value;
+    }
+  }
+
+  if(s == "?"){
+    alert("Please make a selection.");
+    return false;
+  }
+
+  if(s == theAnswer){
+    alert("'"+s+"' is correct! She forgot to put the line #TouhouDanmakufu[Single]!");
+  }else if(s == "A"){
+    alert("'"+s+"' is incorrect. Please review Part 2.");
+  }else if(s == "C"){
+    alert("'"+s+"' is incorrect. Please reread the code.");
+  }else{
+    alert("'"+s+"'? Well... I guess it *could* be Yukari's fault, but this is probably not an incident and it is not a good idea to go out and attack someone because of your own mistakes. (Then again, this is Reimu we're talking about, so... hmm...) Anyways, actually choose a legitimate answer next time, OK?");
+  }
+
+  // return "false" to indicate not to
+  // submit the form.
+  // change this to "true" if the form
+  // "action" is valid,
+  // i.e. points to a valid CGI script
+  //
+  return false;
+}
