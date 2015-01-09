f8a99ae7df3f5fca7f57d9da0a77ef784b99957f
Andrew Fan <andrewsuicune@gmail.com>
2015-01-09 14:29:53 -0500

    Lesson 7 and ObjMove

---
+++ ph3tutorials/javascript/U1L7Quiz1.js
@@ -0,0 +1,27 @@
+function checkAnswerA1(quizForm, theAnswer){
+  var s = "?";
+
+  var i = 0;
+  for(;i<quizForm.elements.length;i++){
+    if(("cc" == quizForm.elements[i].name) && (quizForm.elements[i].checked)){
+      s = quizForm.elements[i].value;
+    }
+  }
+
+  if(s == "?"){
+    alert("Please make a selection.");
+    return false;
+  }
+
+  if(s == theAnswer){
+    alert("'"+s+"' is correct! By reaching the destination at a certain speed, it is guaranteed that everything will sync!");
+  }else if(s == "A"){
+    alert("'"+s+"' is incorrect. This function would most likely teleport the boss onto the player, resulting in a very cheap autokill unless the player was notified of the teleportation beforehand.");
+  }else if(s == "B"){
+    alert("'"+s+"' is incorrect. This function has different movement times depending on how far away the player is.");
+  }else{
+    alert("'"+s+"' is a possible answer, but you'll be sitting there for a while trying to figure out how to time everything perfectly.");
+  }
+
+  return false;
+}
