520da3a85ca2ac81e2508c2e01bf226ce1c761de
Andrew Fan <andrewsuicune@gmail.com>
2015-04-29 10:11:59 -0400

    Parts of Lesson 1

---
+++ classresources/jhu_378116/javascript/JapQuiz.js
@@ -0,0 +1,59 @@
+function checkAnswerGENERIC(quizForm, theAnswer){
+  var s = "?";
+
+  var i = 0;
+  for(;i<quizForm.elements.length;i++){
+    if(("cc" == quizForm.elements[i].name) && (quizForm.elements[i].checked)){
+      s = quizForm.elements[i].value;
+    }
+  }
+
+  if(s == "?"){
+    alert("Please make a selection.");
+    return false;
+  }
+
+  if(s == theAnswer){
+    alert("'"+s+"' is correct!");
+  }else{
+    alert("'"+s+"' is incorrect.");
+  }
+
+  // return "false" to indicate not to
+  // submit the form.
+  // change this to "true" if the form
+  // "action" is valid,
+  // i.e. points to a valid CGI script
+  //
+  return false;
+}
+
+function checkAnswerCA(quizForm, theAnswer, correctAnswer){
+  var s = "?";
+
+  var i = 0;
+  for(;i<quizForm.elements.length;i++){
+    if(("cc" == quizForm.elements[i].name) && (quizForm.elements[i].checked)){
+      s = quizForm.elements[i].value;
+    }
+  }
+
+  if(s == "?"){
+    alert("Please make a selection.");
+    return false;
+  }
+
+  if(s == theAnswer){
+    alert("'"+s+"' is correct!");
+  }else{
+    alert("'"+s+"' is incorrect. The correct answer is" + correctAnswer);
+  }
+
+  // return "false" to indicate not to
+  // submit the form.
+  // change this to "true" if the form
+  // "action" is valid,
+  // i.e. points to a valid CGI script
+  //
+  return false;
+}
