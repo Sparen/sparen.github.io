8b2476e3c06a243dc1b2301ec58a5b0bb3c7d329
Andrew Fan <andrewsuicune@gmail.com>
2015-12-31 14:12:41 -0500

    Lesson 1 translated into Spanish

--- ph3tutorials/javascript/U1L1Quiz1.js
+++ ph3tutorials/javascript/U1L1Quiz1.js
@@ -21,3 +21,27 @@ function checkAnswer(quizForm, theAnswer){
 
   return false;
 }
+
+function checkAnswerESP(quizForm, theAnswer){
+  var s = "?";
+
+  var i = 0;
+  for(;i<quizForm.elements.length;i++){
+    if(("cc" == quizForm.elements[i].name) && (quizForm.elements[i].checked)){
+      s = quizForm.elements[i].value;
+    }
+  }
+
+  if(s == "?"){
+    alert("Por favor haga una selección.");
+    return false;
+  }
+
+  if(s == theAnswer){
+    alert("¡La " + "'"+s+"' es correcta! No puedes ejecutar scripts de ph3 en 0.12m.");
+  }else{
+    alert("La " + "'"+s+"' es incorrecta. Por favor vuelve a revisar la parte 5.");
+  }
+
+  return false;
+}
