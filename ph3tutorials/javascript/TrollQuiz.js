e987019feb20f0873721880a12fad075112bb33b
Andrew Fan <andrewsuicune@gmail.com>
2016-02-22 21:30:20 -0500

    Removed some unnecessary comments

--- ph3tutorials/javascript/TrollQuiz.js
+++ ph3tutorials/javascript/TrollQuiz.js
@@ -18,12 +18,6 @@ function checkAnswerTROLL(quizForm, theAnswer){
   }else{
     alert("Thread 0 Crashed:: Dispatch queue: com.apple.main-thread0 com.apple.WebCore 0x0000000108446f1e WTF::HashMap<unsigned long, WTF::RefPtr<WebCore::ResourceLoader>, WTF::IntHash<unsigned long>, WTF::HashTraits<unsigned long>, WTF::HashTraits<WTF::RefPtr<WebCore::ResourceLoader> > >::remove(unsigned long const&) + 14\n1 com.apple.WebCore 0x00000001081f5df1 WebCore::DocumentLoader::removePlugInStreamLoader(WebCore::ResourceLoader*) + 33\n2 com.apple.WebCore 0x00000001089fe03c WebCore::NetscapePlugInStreamLoader::didFail(WebCore::ResourceError const&) + 28\n3 com.apple.WebKit 0x000000010728ad32 WebKit::WebResourceLoadScheduler::internallyFailedLoadTimerFired() + 262\n4 com.apple.JavaScriptCore 0x0000000107e06603 WTF::RunLoop::TimerBase::timerFired(__CFRunLoopTimer*, void*) + 35\n\n\n\n\n...Just Joking");
   }
-
-  // return "false" to indicate not to
-  // submit the form.
-  // change this to "true" if the form
-  // "action" is valid,
-  // i.e. points to a valid CGI script
-  //
+  
   return false;
 }
