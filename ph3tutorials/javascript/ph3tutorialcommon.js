9d0b96ec4ea2a21b41eab964f7f0482cc0fbc4e1
Andrew Fan <andrewsuicune@gmail.com>
2018-01-07 00:04:26 -0500

    Implemented common footer functionality in ph3 tutorials; all spanish language tutorials ported over

---
+++ ph3tutorials/javascript/ph3tutorialcommon.js
@@ -0,0 +1,30 @@
+function setupENG(lastupd){
+  var newfooter = '';
+  if (lastupd !== "") {
+    newfooter += '<p style="text-align: center;">Last Update to Page: ' + lastupd + '</p>';
+  }
+  newfooter += '<p style="text-align: center;"><a href="#top">Top</a></p>' + 
+    '<p style="text-align: center;">Copyright © 2014-2018 by Andrew Fan</p>' + 
+    '<p style="text-align: center;"><span style="font-weight: bold;">Other Sites/Affiliates</span></p>' + 
+    '<div style="text-align: center;">' + 
+    '<a target="_blank" href="http://www.shrinemaiden.org/forum/index.php"><img alt="&quot;Maidens of the Kaleidoscope&quot;" src="../images/afflogomotk.png"></a>' + 
+    '<a target="_blank" href="http://www.bulletforge.org"><img alt="&quot;Bulletforge&quot;" src="../images/afflogobulletforge.png"></a>' + 
+    '</div>';
+  document.getElementById("footer").innerHTML = newfooter;
+}
+
+function setupESP(lastupd){
+  var newfooter = '';
+  if (lastupd !== "") {
+    newfooter += '<p style="text-align: center;">Última actualización de la página: ' + lastupd + '</p>';
+  }
+  newfooter += '<p style="text-align: center;"><a href="#top">Top</a></p>' + 
+    '<p style="text-align: center;">Copyright © 2014-2018 por Andrew Fan</p>' + 
+    '<p style="text-align: center;">Esta página fue traducido por Paty y Andrew Fan.</p>' +
+    '<p style="text-align: center;"><span style="font-weight: bold;">Otros Sitios/Afiliados</span></p>' + 
+    '<div style="text-align: center;">' + 
+    '<a target="_blank" href="http://www.shrinemaiden.org/forum/index.php"><img alt="&quot;Maidens of the Kaleidoscope&quot;" src="../images/afflogomotk.png"></a>' + 
+    '<a target="_blank" href="http://www.bulletforge.org"><img alt="&quot;Bulletforge&quot;" src="../images/afflogobulletforge.png"></a>' + 
+    '</div>';
+  document.getElementById("footer").innerHTML = newfooter;
+}
