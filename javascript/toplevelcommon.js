e730b94602f812232ca6d27965c0884faa3bf0da
Andrew Fan <andrewsuicune@gmail.com>
2020-02-25 21:56:14 -0500

    New top level alert system

---
+++ javascript/toplevelcommon.js
@@ -0,0 +1,5 @@
+// Set an alert
+document.getElementById("alert_div").innerHTML = '⚠️ Danmakufu Wiki is down as MotK\'s database is extracted. ⚠️<br>For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>.';
+
+// Disable alerts
+//document.getElementById("alert_div").parentNode.removeChild(document.getElementById("alert_div"));
