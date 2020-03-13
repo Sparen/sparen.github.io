8bd045139a9995392945625461535abd2947387f
Andrew Fan <andrewsuicune@gmail.com>
2020-03-13 16:44:10 -0400

    Adjusted Alert div so that it can be properly hidden without having to destroy it after rendering the page

--- javascript/toplevelcommon.js
+++ javascript/toplevelcommon.js
@@ -1,5 +1,2 @@
 // Set an alert
-document.getElementById("alert_div").innerHTML = '⚠️ Danmakufu Wiki is down as MotK\'s database is extracted. ⚠️<br>For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>.';
-
-// Disable alerts
-//document.getElementById("alert_div").parentNode.removeChild(document.getElementById("alert_div"));
+document.getElementById("alert_div").innerHTML = '<div class="alert_inner">⚠️ Danmakufu Wiki is down indefinitely. ⚠️<br>For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>.</div>';
