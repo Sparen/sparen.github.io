1e5190a07f5a7a25092ef4e1887b5c82b6627116
Andrew Fan <andrewsuicune@gmail.com>
2020-08-07 12:27:23 -0400

    Added BHA2 advertisement using the new alert banner feature

--- javascript/toplevelcommon.js
+++ javascript/toplevelcommon.js
@@ -1,2 +1,5 @@
 // Set an alert
-document.getElementById("alert_div").innerHTML = '<div class="alert_inner">⚠️ Danmakufu Wiki is down indefinitely. ⚠️<br>For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>.</div>';
+document.getElementById("alert_div").innerHTML = `
+<div class="notice_inner_sm"><a href="https://www.discord.gg/TYvazgV"><img src="https://sparen.github.io/projects/contest/bha2_banner.png" style="max-height:96px"></a><br>
+Bullet Hell Artist Contest #2 ongoing until Sept 6, 2020 on the Bullet Hell Engines Discord Server!</div>
+<div class="alert_inner_sm">⚠️ Danmakufu Wiki is down indefinitely. For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>. ⚠️</div>`;
