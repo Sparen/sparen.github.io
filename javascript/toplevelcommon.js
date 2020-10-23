2867d379b8e3e1e9e002e6058211340bdfa08bdf
Andrew Fan <andrewsuicune@gmail.com>
2020-10-22 23:55:57 -0400

    Fix typo in toplevelcommon.js

--- javascript/toplevelcommon.js
+++ javascript/toplevelcommon.js
@@ -1,7 +1,7 @@
 // Set an alert
 document.getElementById("alert_div").innerHTML = `
 <div class="notice_inner_sm">
-Bullet Hell Artist Contest #2 has concluded! See the Contest Database for results!
+Bullet Hell Artistry Contest #2 has concluded! See the Contest Database for results!
 </div>
 <div class="alert_inner_sm">
 ⚠️ Danmakufu Wiki is down indefinitely. For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>. ⚠️
