72dd816d9412994b6bf9e699fd6154898b498bfa
Andrew Fan <andrewsuicune@gmail.com>
2020-12-23 13:35:55 -0500

    Remove BHE2 banner

--- javascript/toplevelcommon.js
+++ javascript/toplevelcommon.js
@@ -1,8 +1,5 @@
 // Set an alert
 document.getElementById("alert_div").innerHTML = `
-<div class="notice_inner_sm">
-Bullet Hell Artistry Contest #2 has concluded! See the Contest Database for results!
-</div>
 <div class="alert_inner_sm">
 ⚠️ Danmakufu Wiki is down indefinitely. For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>. ⚠️
 </div>`;
