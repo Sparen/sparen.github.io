77b283495446298e34d756941378e06d835f9222
Andrew Fan <andrewsuicune@gmail.com>
2021-01-16 18:14:29 -0500

    Add BHE 3 banner

--- javascript/toplevelcommon.js
+++ javascript/toplevelcommon.js
@@ -1,5 +1,9 @@
 // Set an alert
 document.getElementById("alert_div").innerHTML = `
+<div class="notice_inner_sm">
+<img src="./projects/contest/bha3_banner.png" style="max-width: 50%"><br>
+Bullet Hell Artistry Contest #3 - Unexpected Mastermind 2 is now ongoing! Visit <a target="_blank" href="https://discord.gg/4wNvvPHxeU">BHE Discord</a> for more information!
+</div>
 <div class="alert_inner_sm">
 ⚠️ Danmakufu Wiki is down indefinitely. For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>. ⚠️
 </div>`;
