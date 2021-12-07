e9b04a3d0d94f6f0686a559821420d7132ff1b36
Andrew Fan <andrewsuicune@gmail.com>
2021-12-06 19:08:27 -0500

    Add BHA4 judging

--- javascript/toplevelcommon.js
+++ javascript/toplevelcommon.js
@@ -1,7 +1,5 @@
 // Set an alert
 document.getElementById("alert_div").innerHTML = `
-<div class="notice_inner_sm">
-BHA#4 Contest!<br><img style="max-width: 320px;" src="https://sparen.github.io/projects/contest/bha4_banner.png"></div>
 <div class="alert_inner_sm">
 ⚠️ Danmakufu Wiki is down indefinitely. For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>. ⚠️
 </div>`;
