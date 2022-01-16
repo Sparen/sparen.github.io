b8a56850cd65e22a6cc3aa9c99dac52f5759b92d
Andrew Fan <andrewsuicune@gmail.com>
2022-01-16 11:24:25 -0500

    Add absolute path in alert message for BHA5

--- javascript/toplevelcommon.js
+++ javascript/toplevelcommon.js
@@ -1,7 +1,7 @@
 // Set an alert
 document.getElementById("alert_div").innerHTML = `
 <div class="notice_inner_sm">
-<img src="./projects/contest/bha5_banner.png" style="max-width: 100%; max-height: 128px"><br>Bullet Hell Artistry Contest #5 ongoing until March 1, 2022!
+<img src="http://sparen.github.io/projects/contest/bha5_banner.png" style="max-width: 100%; max-height: 128px"><br>Bullet Hell Artistry Contest #5 ongoing until March 1, 2022!
 </div>
 <div class="alert_inner_sm">
 ⚠️ Danmakufu Wiki is down indefinitely. For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>. ⚠️
