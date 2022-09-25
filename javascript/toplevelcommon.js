3c1074e26b9a7f72bc42e053aad93db582a3a623
Andrew Fan <andrewsuicune@gmail.com>
2022-09-25 11:33:17 -0400

    Stage prep for BHA6

--- javascript/toplevelcommon.js
+++ javascript/toplevelcommon.js
@@ -2,4 +2,8 @@
 document.getElementById("alert_div").innerHTML = `
 <div class="alert_inner_sm">
 ⚠️ Danmakufu Wiki is down indefinitely. For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>. ⚠️
+</div>
+<div class="notice_inner_sm">
+Bullet Hell Artistry Contest #6 is now open until October 30, 2022!<br>
+<img src="https://sparen.github.io/projects/contest/bha6_banner.png" style="max-width: 100%">
 </div>`;
