728f0c012846d11640f85d11adce4e6ec83ad7c8
Andrew Fan <andrewsuicune@gmail.com>
2023-07-01 19:31:08 -0400

    Add initial BHA 7 contest information

--- javascript/toplevelcommon.js
+++ javascript/toplevelcommon.js
@@ -4,6 +4,6 @@ document.getElementById("alert_div").innerHTML = `
 ⚠️ Danmakufu Wiki is down indefinitely. For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>. ⚠️
 </div>
 <div class="notice_inner_sm">
-Air Fryer Central Contest #1 is now open until January 24, 2023!<br>
-<img src="https://sparen.github.io/projects/contest/afc1_banner.png" style="max-height: 128px">
+Bullet Hell Artistry Contest #7 is now open until August 15, 2023!<br>
+<img src="https://sparen.github.io/projects/contest/bha7_banner.png" style="max-height: 128px">
 </div>`;
