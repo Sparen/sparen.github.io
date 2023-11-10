51df9c091877f2af3cf83681b77ede3cbb5bab49
Andrew Fan <andrewsuicune@gmail.com>
2023-11-09 23:01:32 -0500

    Add BHA7 judging and results

--- javascript/toplevelcommon.js
+++ javascript/toplevelcommon.js
@@ -3,7 +3,11 @@ document.getElementById("alert_div").innerHTML = `
 <div class="alert_inner_sm">
 ⚠️ Danmakufu Wiki is down indefinitely. For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>. ⚠️
 </div>
+`;
+
+/* -- Sample contest banner to add after the alert
 <div class="notice_inner_sm">
 Bullet Hell Artistry Contest #7 is now open until August 15, 2023!<br>
 <img src="https://sparen.github.io/projects/contest/bha7_banner.png" style="max-height: 128px">
-</div>`;
+</div>
+*/
