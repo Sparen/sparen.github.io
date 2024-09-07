8b93190d915f1691824233f1f2d7a5e00c511589
Andrew Fan <andrewsuicune@gmail.com>
2024-09-06 21:25:39 -0400

    Remove BHA 8 banner

--- javascript/toplevelcommon.js
+++ javascript/toplevelcommon.js
@@ -3,15 +3,11 @@ document.getElementById("alert_div").innerHTML = `
 <div class="alert_inner_sm">
 ⚠️ Danmakufu Wiki is down indefinitely. For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>. ⚠️
 </div>
-<div class="notice_inner_sm">
-Bullet Hell Artistry Contest #8 is now open until August 31, 2024!<br>
-<img src="https://sparen.github.io/projects/contest/bha8_banner.png" style="max-height: 128px">
-</div>
 `;
 
 /* -- Sample contest banner to add after the alert
 <div class="notice_inner_sm">
-Bullet Hell Artistry Contest #7 is now open until August 15, 2023!<br>
-<img src="https://sparen.github.io/projects/contest/bha7_banner.png" style="max-height: 128px">
+Bullet Hell Artistry Contest #8 is now open until August 31, 2024!<br>
+<img src="https://sparen.github.io/projects/contest/bha8_banner.png" style="max-height: 128px">
 </div>
 */
