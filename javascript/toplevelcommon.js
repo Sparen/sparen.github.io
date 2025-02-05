8cf688528325c5b0ab1f0605dcde18cb26ffad3d
Andrew Fan <andrewsuicune@gmail.com>
2025-02-04 22:51:36 -0500

    Add BHA9 description and banner

--- javascript/toplevelcommon.js
+++ javascript/toplevelcommon.js
@@ -3,6 +3,10 @@ document.getElementById("alert_div").innerHTML = `
 <div class="alert_inner_sm">
 ⚠️ Danmakufu Wiki is down indefinitely. For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>. ⚠️
 </div>
+<div class="notice_inner_sm">
+Bullet Hell Artistry Contest #9 is now open until March 01, 2025!<br>
+<img src="https://sparen.github.io/projects/contest/bha9_banner.png" style="max-height: 128px">
+</div>
 `;
 
 /* -- Sample contest banner to add after the alert
