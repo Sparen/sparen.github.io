ffc976f726d289b75f22f5d5cdf77c6c6b19bd3a
Andrew Fan <andrewsuicune@gmail.com>
2025-03-14 23:58:55 -0400

    Initial notes for BHA9; disabled alert since it's been years

--- javascript/toplevelcommon.js
+++ javascript/toplevelcommon.js
@@ -1,13 +1,10 @@
 // Set an alert
-document.getElementById("alert_div").innerHTML = `
-<div class="alert_inner_sm">
-⚠️ Danmakufu Wiki is down indefinitely. For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>. ⚠️
-</div>
-<div class="notice_inner_sm">
-Bullet Hell Artistry Contest #9 is now open until March 01, 2025!<br>
-<img src="https://sparen.github.io/projects/contest/bha9_banner.png" style="max-height: 128px">
-</div>
-`;
+document.getElementById("alert_div").innerHTML = ``;
+
+// -- Original Danmakufu Wiki notice
+//<div class="alert_inner_sm">
+//⚠️ Danmakufu Wiki is down indefinitely. For Wiki content not backed up on this site, use the <a target="_blank" href="https://web.archive.org/web/*/http://dmf.shrinemaiden.org/wiki/Main_Page">Wayback Machine</a>. ⚠️
+//</div>
 
 /* -- Sample contest banner to add after the alert
 <div class="notice_inner_sm">
